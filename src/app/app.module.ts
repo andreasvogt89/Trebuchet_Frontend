d8167158050ec4c885fefda4675be622d353869b
andreasvogt89 <30302212+andreasvogt89@users.noreply.github.com>
2020-03-28 18:26:01 +0100

    -Websocket implemented

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/layout/header/header.component';
+import { AboutComponent } from './components/pages/about/about.component';
+import {MystromComponent} from './components/devcies/mystrom/mystrom.component';
+import {HttpClientModule} from '@angular/common/http';
+import {HttpDataService} from './services/http/HttpDataServcie';
 
-@NgModule({
+@NgModule( {
   declarations: [
-    AppComponent
+    AppComponent,
+    MystromComponent,
+    HeaderComponent,
+    AboutComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
   ],
-  providers: [],
+  providers: [
+    HttpDataService,
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
+
---
+++ src/app/components/models/myStromData.ts
@@ -0,0 +1,11 @@
+export class MyStromData {
+  id: number;
+  timestamp: any;
+  power: number;
+  energy: number;
+  relay: boolean;
+  temperature: number;
+  deviceName: string;
+  constructor() {
+  }
+}
